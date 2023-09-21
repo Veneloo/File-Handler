4d073be87811eee6cb5a0e4068576e57e4c5b129
Veneloo <axel.cazorla10@gmail.com>
2023-09-20 20:06:19 -0400

    Added Introduction and Initial Libraries

---
+++ Script.py
@@ -0,0 +1,12 @@
+####################################################################################################
+##  Author: Axel Cazorla                                                                          ##
+##  Date Created: 9/20/2023                                                                       ##
+##                                                                                                ##
+##  This Python scripts automates any downloaded file into a folder depending the file type.      ##
+##                                                                                                ##
+####################################################################################################         
+
+import os
+import shutil
+import time
+
